1585e72231bb388e9bd2290755c716c4893efa97
marc0dev0417 <marcobenegasdev@outlook.es>
2022-09-17 21:41:53 +0200

    WB/02: added listeners to user text and delete messages

--- src/index.js
+++ src/index.js
@@ -2,16 +2,23 @@ const fs = require('node:fs')
 const path = require('node:path')
 const { Client, Intents, Collection } = require('discord.js');
 const { token, guildId, clientId } = require('../config.json');
+const { GatewayIntentBits } = require('discord-api-types/v9');
+const { channel } = require('node:diagnostics_channel');
 
 // Create a new client instance
-const client = new Client({ intents: [Intents.FLAGS.GUILDS] });
+const client = new Client(
+	{ 
+	partials: ['CHANNEL', 'MESSAGE', 'REACTION'],
+	intents: [Intents.FLAGS.GUILDS, Intents.FLAGS.GUILD_MESSAGES, GatewayIntentBits.Guilds, GatewayIntentBits.GuildMessages] 
+	}
+)
 client.commands = new Collection()
 
 
 // When the client is ready, run this code (only once)
 client.once('ready', () => {
 	console.log('Ready!');
-});
+})
 
 const commandsPath = path.join(__dirname, 'commands')
 const commandFiles = fs.readdirSync(commandsPath).filter(file => file.endsWith('.js'))
@@ -37,6 +44,23 @@ client.on('interactionCreate', async interaction => {
 		console.error(error);
 		await interaction.reply({ content: 'There was an error while executing this command!', ephemeral: true });
 	}
-});
+})
+
+client.on('messageCreate', async(message) => {
+	if(message.author.bot) return
+
+	if(message.content === 'hello'){
+		await message.channel.send('hello my friend')
+	}
+})
+
+client.on('messageCreate', (message) => {
+	if(message.author.bot) return
+
+	if(message.content === '/deleteMessages'){
+		message.channel.bulkDelete(100)
+	}
+})
+
 // Login to Discord with your client's token
 client.login(token);
